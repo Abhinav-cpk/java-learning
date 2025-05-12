5ccf663c95529cad1f3007dfc7729f91bb54940c
Abhinav-cpk <abhinavtyagi9451@gmail.com>
2025-05-12 15:42:58 +0530

    Session 2 completed

--- Operators,if-else and numbersystem/Assignment.java
+++ Operators,if-else and numbersystem/Assignment.java
@@ -2,8 +2,8 @@ public class Assignment {
     public static void main(String[] args) {
       int myInt = 9;
       System.out.println(myInt);
-      int yourNum = myInt;
-      System.out.println(yourNum);
+      int yourInt = myInt;
+      System.out.println(yourInt);
       // 9 = myInt (shows error as the assignment operator assigns the value at right to the value at left)
     }
 }
--- Operators,if-else and numbersystem/ChallengeSwap.java
+++ Operators,if-else and numbersystem/ChallengeSwap.java
@@ -4,14 +4,14 @@ public class ChallengeSwap {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your first number: ");
-        int firstNum = input.nextInt();
+        int a = input.nextInt();
         System.out.println("Enter your second number: ");
-        int secNum = input.nextInt();
-        System.out.println("The numbers are " + firstNum + " " + secNum);
-        int c = firstNum;
-        firstNum = secNum;
-        secNum = c;
-        System.out.println("The numbers after swapping are " + firstNum + " " + secNum);
+        int b = input.nextInt();
+        System.out.println("The numbers are " + a + " " + b);
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("The numbers after swapping are " + a + " " + b);
 
     }
 }
