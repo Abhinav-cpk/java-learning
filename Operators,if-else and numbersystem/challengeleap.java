2408d97671043922fa4ae4c0bb8a00fa623e1cf6
Abhinav-cpk <abhinavtyagi9451@gmail.com>
2025-05-13 15:37:30 +0530

    Session 3 completed

--- Operators,if-else and numbersystem/challengeleap.java
+++ Operators,if-else and numbersystem/challengeleap.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class challengeleap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int year;
+        while(true) {
+            System.out.print("Enter the year you want to check is a leap year or not, else type exit to quit: ");
+            if (input.hasNextInt()) {
+                year = input.nextInt();
+
+                if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
+                    System.out.println("The entered year is a leap year.");
+                } else {
+                    System.out.println("The entered year is not a leap year.");
+                }
+                //break;
+            } else {
+                String temp = input.next().trim();
+                if (temp.equalsIgnoreCase("Exit")) {
+                    System.out.println("Exiting the program.");
+                    break;
+                } else {
+                    System.out.println("Enter a valid year in numbers.");
+                }
+            }
+        }
+    input.close();
+    }
 }
